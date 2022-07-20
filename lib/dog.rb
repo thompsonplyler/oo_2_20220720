a4ad0c37e8f3e792a2e3288d558497bb06a745a5
Thompson Plyler <nealok.g@gmail.com>
2022-07-20 13:14:48 -0400

    Added notes for Dog class to guide lecture.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,27 @@
 require 'pry'
-require_relative 'dog_data'
+
+#
+# create dog with name, age, breed, image_url, last_fed, last_walked
+#
 
 class Dog
-    attr_accessor :name, :age, :breed
+  attr_accessor :name, :age, :breed
+  # ✅ 1. class variable: all - holds all dogs we have saved
+  # ✅ 2. class method: 'all' - retrieves the value of the class variable
 
-    def initialize attributes={}
-            attributes.each do |attr, value|
-                self.send("#{attr}=", value)
-            end
-    end
+  def initialize attributes={}
+        attributes.each do |attr, value|
+            self.send("#{attr}=", value)
+        end
+  end
 
-    # create dog with name, age, image_url
-    # access name age breed, image_url, lastfed, last walked times.
-    # print details about a dog, including last walked and last fed.
-
+  # ✅ 3. instance method: 'save' - saves this dog instance to our class variable
+  # ✅ 4. class method: 'create' - creates a new instances and saves it to the class variable
+  # ✅ 5. instance method: 'walk' - updates the dog's last_walked property to the current time
+  # ✅ 6. instance method: 'feed' - updates the dog's last_fed property to the current time
+  # ✅ 7. print details about a dog, including last walked and last fed.
+  # ✅ 8. private method #formatted name
+  # ✅ 9. private method #formatted_time
 end
 
 binding.pry
